281e9cb36edce8ce22228079526a9ff86092f3e6
informaticosonline <109153512+informaticosonline@users.noreply.github.com>
2023-12-29 22:32:57 +0100

    Initial commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,84 @@
+<!-- Enlace al archivo CSS -->
+<link rel="stylesheet" href="navegacion.css">
+
+<!-- Navegación -->
+<nav>
+    <ul>
+        <!-- Productos -->
+        <li><a href="#">Productos</a>
+            <ul>
+                <!-- Cajas -->
+                <li><a href="#">Cajas</a></li>
+
+                <!-- Líquidos -->
+                <li><a href="#">Líquidos</a>
+                    <ul>
+                        <li><a href="#">Sales de nicotina</a>
+                            <ul>
+                                <li><a href="#">Mejores valorados</a></li>
+                                <li><a href="#">Todos</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Aromas</a>
+                            <ul>
+                                <li><a href="#">Mejores valorados</a></li>
+                                <li><a href="#">Todos</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Nicotina</a>
+                            <ul>
+                                <li><a href="#">Mejores valorados</a></li>
+                                <li><a href="#">Todos</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- Pods -->
+                <li><a href="#">Pods</a>
+                    <ul>
+                        <li><a href="#">Pods kit</a></li>
+                        <li><a href="#">Resistencias</a></li>
+                        <li><a href="#">Mejores valorados</a></li>
+                    </ul>
+                </li>
+
+                <!-- Pods Desechables -->
+                <li><a href="#">Pods Desechables</a>
+                    <ul>
+                        <li><a href="#">Marcas</a></li>
+                        <li><a href="#">Mejores valorados</a></li>
+                    </ul>
+                </li>
+
+                <!-- Mods y Kits -->
+                <li><a href="#">Mods y Kits</a>
+                    <ul>
+                        <li><a href="#">Atomizadores</a></li>
+                        <li><a href="#">Accesorios</a></li>
+                        <!-- Más subcategorías aquí -->
+                    </ul>
+                </li>
+
+                <!-- Cachimbas -->
+                <li><a href="#">Cachimbas</a>
+                    <ul>
+                        <li><a href="#">Cachimbas</a></li>
+                        <li><a href="#">Accesorios</a>
+                            <ul>
+                                <li><a href="#">Mangueras</a></li>
+                                <!-- Más subcategorías aquí -->
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+
+        <!-- Otras secciones -->
+        <li><a href="#">Novedades</a></li>
+        <li><a href="#">Promociones</a></li>
+        <li><a href="#">Próximamente</a></li>
+        <li><a href="#">Sobre nosotros</a></li>
+    </ul>
+</nav>
---
+++ resources/views/mantenimiento.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HumoVibe</title>
+    <link rel="icon" href="{{ asset('images/LogotipoIco.webp') }}" type="image/webp">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/mantenimiento.css') }}">
+</head>
+
+<body class="mi-div-de-fondo">
+
+            <h1>HumoVibe</h1>
+
+            <img src="{{ asset('images/LogotipoEscala.webp') }}" alt="Logotipo Grande">
+
+            <h3>Página en construccion</h3>
+            <h4>Estaremos en una calada</h4>
+
+            <a href="https://www.tiktok.com/@humovibe" class="tiktok-icon" target="_blank">
+                <i class="fab fa-tiktok" style="color:#aaca74"></i>
+            </a>          
+
+            <a href="https://www.instagram.com/humo_vibe?igsh=dmw2b2Ztc2gyc3h6" target="_blank">
+                <i class="fab fa-instagram-square" style="color:#f018a3"></i>
+            </a>
+
+            <a href="https://www.facebook.com/profile.php?id=61554982893632" target="_blank">
+                <i class="fab fa-facebook-square" style="color:#aaca74"></i>
+            </a>
+
+            <a href="https://twitter.com/HumoVibe3670" class="twitter-icon" target="_blank">
+                <i class="fab fa-twitter" style="color:#f018a3"></i>
+            </a>
+            
+            <a href="https://www.youtube.com/channel/UCykYzNnb4vWcafqcmyC-dPg" target="_blank">
+                <i class="fab fa-youtube-square" style="color: #aaca74"></i>
+            </a>
+
+            <a href="https://t.me/34614180994" class="telegram-icon" target="_blank">
+                <i class="fab fa-telegram-plane"></i>
+            </a>
+
+            <a href="https://wa.me/34614180994" target="_blank" class="whatsapp-icon">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+            
+            <p>&copy; {{ \Carbon\Carbon::now()->format('m/Y') }} - Todos los derechos reservados. Realizada por 
+                <a href="https://informaticosonline.es/" target="_blank">
+                    <img src="{{ asset('images/nuestroLogoPeque.png') }}" alt="Informaticos Online">
+                </a>
+            </p>
+
+</body>
